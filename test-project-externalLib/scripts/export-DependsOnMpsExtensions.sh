a83d6de851d5ba03247ef743b278a3778aa3da52
Niko Stotz <niko@nikostotz.de>
2025-02-11 13:32:31 +0100

    trial to hide subproject build models

--- test-project-externalLib/scripts/export-DependsOnMpsExtensions.sh
+++ test-project-externalLib/scripts/export-DependsOnMpsExtensions.sh
@@ -2,6 +2,11 @@
 
 OUTPUT_FILE=DependsOnMpsExtensions.lw-lang.json
 
+mv solutions/testProjectExternalLib.build/models/testProjectExternalLib.build.mps-ignore solutions/testProjectExternalLib.build/models/testProjectExternalLib.build.mps
+
 ./gradlew build runCommandLineTool -Pargs=". -lDependsOnMpsExtensions-lang actual/$OUTPUT_FILE -mlionweb-mps.home=./../ -s=fineGrainedClosure"
+
+mv solutions/testProjectExternalLib.build/models/testProjectExternalLib.build.mps solutions/testProjectExternalLib.build/models/testProjectExternalLib.build.mps-ignore
+
 
 diff actual/$OUTPUT_FILE expected/$OUTPUT_FILE
