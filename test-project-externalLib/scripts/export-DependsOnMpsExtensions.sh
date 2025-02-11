dded2921f963eff54273668c8566863fa034e257
Niko Stotz <niko@nikostotz.de>
2025-02-11 13:48:10 +0100

    use build model disguise for all scripts

--- test-project-externalLib/scripts/export-DependsOnMpsExtensions.sh
+++ test-project-externalLib/scripts/export-DependsOnMpsExtensions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 OUTPUT_FILE=DependsOnMpsExtensions.lw-lang.json
+BUILD_MODEL=solutions/testProjectExternalLib.build/models/testProjectExternalLib.build
 
-mv solutions/testProjectExternalLib.build/models/testProjectExternalLib.build.mps-ignore solutions/testProjectExternalLib.build/models/testProjectExternalLib.build.mps
+mv $BUILD_MODEL.mps-ignore $BUILD_MODEL.mps
 
 ./gradlew build runCommandLineTool -Pargs=". -lDependsOnMpsExtensions-lang actual/$OUTPUT_FILE -mlionweb-mps.home=./../ -s=fineGrainedClosure"
 
-mv solutions/testProjectExternalLib.build/models/testProjectExternalLib.build.mps solutions/testProjectExternalLib.build/models/testProjectExternalLib.build.mps-ignore
-
+mv $BUILD_MODEL.mps $BUILD_MODEL.mps-ignore
 
 diff actual/$OUTPUT_FILE expected/$OUTPUT_FILE
